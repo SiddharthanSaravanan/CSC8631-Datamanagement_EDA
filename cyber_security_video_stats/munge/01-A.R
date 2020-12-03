a7f855adea0a7e06687362ae24fc0874b8865f53
SiddharthanSaravanan <s.saravanan2@newcastle.ac.uk>
2020-12-03 13:27:54 +0000

    Data Preprocessing file

--- cyber_security_video_stats/munge/01-A.R
+++ cyber_security_video_stats/munge/01-A.R
@@ -1,1 +1,23 @@
-# Example preprocessing script.
+#Preprocessing script.
+
+#Adding new column to the first datafile
+cyber.security.3_video.stats$year = 2017
+cyber.security.3_video.stats$month = "September"
+
+#Adding new column to the second datafile
+cyber.security.4_video.stats$year = 2017
+cyber.security.4_video.stats$month = "November"
+
+#combining two datafiles as a single datafile
+video_stats_data = union_all(cyber.security.3_video.stats, cyber.security.4_video.stats)
+
+#Converting the structure of two columns for analysis
+video_stats_data$step_position = as.character(video_stats_data$step_position)
+video_stats_data$month=factor(video_stats_data$month, ordered = TRUE,
+                              levels = c("September","November"))
+
+#Cleaning the data - Removing the columns that has values as 0
+video_stats_data = video_stats_data[, colSums(video_stats_data != 0) > 0]
+
+#Analyse the structure of the data
+str(video_stats_data)
